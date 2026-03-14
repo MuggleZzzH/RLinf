3e00c7f7aea590b5919b443f937dc2464dc02897
Brunch-Life <brunchlife@hotmail.com>
2026-03-14 13:33:19 +0000

    spacemouse fix

--- rlinf/envs/realworld/common/spacemouse/spacemouse_expert.py
+++ rlinf/envs/realworld/common/spacemouse/spacemouse_expert.py
@@ -27,20 +27,16 @@ class SpaceMouseExpert:
     def __init__(self):
         import pyspacemouse
 
-        pyspacemouse.open()
+        self._device = pyspacemouse.open()
 
         self.state_lock = threading.Lock()
         self.latest_data = {"action": np.zeros(6), "buttons": [0, 0]}
-        # Start a thread to continuously read the SpaceMouse state
-        self.thread = threading.Thread(target=self._read_spacemouse)
-        self.thread.daemon = True
+        self.thread = threading.Thread(target=self._read_spacemouse, daemon=True)
         self.thread.start()
 
     def _read_spacemouse(self):
-        import pyspacemouse
-
         while True:
-            state = pyspacemouse.read()
+            state = self._device.read()
             with self.state_lock:
                 self.latest_data["action"] = np.array(
                     [-state.y, state.x, state.z, -state.roll, -state.pitch, -state.yaw]
