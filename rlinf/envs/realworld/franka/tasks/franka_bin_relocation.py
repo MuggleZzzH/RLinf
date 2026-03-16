1dfeb83bcb39d58820064a97941cd94246cbd669
MuggleZzzH <2558818257@qq.com>
2026-03-16 20:56:38 +0800

    Clarify realworld action scaling

--- rlinf/envs/realworld/franka/tasks/franka_bin_relocation.py
+++ rlinf/envs/realworld/franka/tasks/franka_bin_relocation.py
@@ -36,6 +36,7 @@ class BinEnvConfig(FrankaRobotConfig):
     reward_threshold: np.ndarray = field(
         default_factory=lambda: np.array([0.01, 0.01, 0.01, 0.2, 0.2, 0.2])
     )
+    action_scale: np.ndarray | None = None
 
     def __post_init__(self):
         self.compliance_param = {
@@ -83,7 +84,10 @@ class BinEnvConfig(FrankaRobotConfig):
             [0.0, 0.0, self.random_z_range_high, 0.0, 0.0, 0.0]
         )
         self.reward_threshold = np.array(self.reward_threshold)
-        self.action_scale = np.array([0.03, 0.1, 1])
+        if self.action_scale is None:
+            self.action_scale = np.array([0.03, 0.1, 1])
+        else:
+            self.action_scale = np.array(self.action_scale)
         self.ee_pose_limit_min = np.array(
             [
                 self.target_ee_pose[0] - 0.01,
